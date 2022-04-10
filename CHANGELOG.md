81d3374b2018657ea87f8e01fe7936fcc6357d68
sineverba <alessandro.arici@gmail.com>
2022-04-10 14:59:39 +0200

    Get public IP

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 # Next version
 + Add Docker
++ Get public IP
 
 ## 0.1.0
 + First version - forked
--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+import * as http from 'http';
 import App from "./src/App.js";
 
 const app = new App();
 
-update();
+const ip = await getPublicIp();
+console.log(ip);
+//update();
+
+
+async function getPublicIp() {
+    return new Promise((resolve, reject) => {
+        http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, response => {
+            response.on('data', data => {
+                resolve(data.toString());
+            })
+        })
+    });
+}
 
 function update() {
     app.browseZones()
