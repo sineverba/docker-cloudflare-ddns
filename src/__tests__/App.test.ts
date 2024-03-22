823cf9caa1d79b270749da72b760033391dc9225
sineverba <alessandro.arici@gmail.com>
2024-03-22 14:25:46 +0100

    Add Cloudflare

--- src/__tests__/App.test.ts
+++ src/__tests__/App.test.ts
@@ -1,9 +1,17 @@
+import { describe, expect, it } from "vitest";
 import App from "../App";
 
-describe("Test APP", () => {
+describe("Test APP file", () => {
   it("Can set and get the token", () => {
     const app = new App();
-    app.setToken("a1b2c3");
-    expect(app.getToken()).toBe("a1b2c3");
+    app.setToken("aFakeToken");
+    expect(app.getToken()).toBe("aFakeToken");
+  });
+
+  it("Can browse a zone", async () => {
+    const app = new App();
+    app.setToken("aFakeToken");
+    const zones = await app.browseZones(process.env.ZONE as string);
+    expect(zones.result[0].id).toBe("23XhtvhnUu5YdKNeRzg3BQqjx1WwF666");
   });
 });
--- src/__tests__/__mocks__/publicIp.js
+++
@@ -1,5 +0,0 @@
-const publicIp = jest.createMockFromModule("public-ip");
-
-publicIp.publicIpv4 = async () => "1.2.3.4";
-
-module.exports = publicIp;
